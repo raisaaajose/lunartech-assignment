bc67548e6da233bd612903ffe29738c2541e179a
raisaaajose <raisaolaprath@gmail.com>
2025-08-24 17:54:46 +0530

    updated generate.py

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # lunartech-assignment
 
-[More than 90% of benign tumors are found in premenopausal patients who have been operated on, whereas in postmenopausal patients only 60% of tumors are benign](https://ovarianresearch.biomedcentral.com/articles/10.1186/s13048-019-0503-7?utm_source=chatgpt.com)
+Menopause
+[More than 90% of benign tumors are found in premenopausal patients who have been operated on, whereas in postmenopausal patients only 60% of tumors are benign
+
+
+- The prevalence of OC among post-menopausal women is estimated to be 1 in 2500
+
+](https://ovarianresearch.biomedcentral.com/articles/10.1186/s13048-019-0503-7?utm_source=chatgpt.com)
+
+CA-125
+[In cases of ovarian cancer, serum CA125 level may be elevated, but this marker has a low sensitivity in the early stages of ovarian cancer(only reported to be elevated in 23-50% of stage I cases)
+
+- Approximately 20% of EOC patients do not exhibit elevated CA125 levels, ](https://ovarianresearch.biomedcentral.com/articles/10.1186/s13048-019-0503-7?utm_source=chatgpt.com)
+
+[Increased CA125 levels are also reported in other physiological or pathological conditions, such as menstruation, pregnancy, endometriosis and inflammatory diseases of the peritoneu
+
+- upper limit is 35 U/mL in pre and post-menopausal patients.
+
+- the specificity of CA125 for detecting ovarian cancer was 78% 
+
+](https://ovarianresearch.biomedcentral.com/articles/10.1186/s13048-019-0503-7?utm_source=chatgpt.com)
--- generate.py
+++ generate.py
@@ -1,46 +1,61 @@
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
 
 np.random.seed(42)
+n = 500
+diagnosis = np.random.binomial(1, 0.3, n)
 
-n_samples=500
+# assuming 40% of cases are early stage (stage I-III) 
+early_stage_mask = (diagnosis == 1) & (np.random.rand(n) < 0.4) 
+age = np.random.normal(50, 10, n).astype(int)
+brca = np.random.binomial(1, np.where(diagnosis == 1, 0.2, 0.1), n)
 
-cancer_rate=0.3
-n_cancer=int(n_samples*cancer_rate)
-n_healthy=n_samples-n_cancer
+#menopausal status flexible as in real-life
+menopause = np.random.binomial(1,
+                    np.where(age > 60, 1.0,
+                    np.where(age > 50, 0.95,
+                    np.where(age > 40,0.65, 0.0))),n)
 
-#creating all columns
+#tumor size also depends on stage of cancer
+tumor_size = np.where(diagnosis == 1,
+                    np.random.normal(6, 1.5, n),
+                    np.random.normal(3, 0.8, n))
+tumor_size[early_stage_mask] -= np.random.normal(1.5, 1.0, early_stage_mask.sum()) #additive subtraction to prevent overwriting of existing values
 
-Diagnosis=np.array([1]*n_cancer+[0]*n_healthy)
-np.random.shuffle(Diagnosis)
+#CA-125 base values
+ca125 = np.where(diagnosis == 1,
+                    np.random.normal(200, 100, n),
+                    np.random.normal(20, 10, n))
 
-Age=np.random.normal(loc=55,scale=10,size=n_samples).clip(20,90)
+# young, BRCA+ populations secrete less CA-125 at early stage levels (Source: Dochez et al., 2019 (BMC Ovarian Research); Nature Reviews)
+low_ca125_mask = (diagnosis == 1) & (age < 45) & (brca == 1) 
+ca125[low_ca125_mask] -= np.random.normal(80, 30, low_ca125_mask.sum())
 
-BMI=np.random.normal(loc=26,scale=4,size=n_samples).clip(15,45)
+#early stage populations also secrete less CA-125 based on stage,specifically which early stage
+subtraction_choices = np.random.choice([30, 40, 50], size=early_stage_mask.sum(), p=[1/3, 1/3, 1/3])
+ca125[early_stage_mask] -= np.random.normal(loc=subtraction_choices, scale=5, size=early_stage_mask.sum())
 
-#to fix:make menopausal status more flexible
-Menopausal=np.where(Age>=50,1,0)
+ca125 = np.clip(ca125, 5, 1000).astype(int) #can reach 1000+
 
-BRCA_rate=0.2
-n_BRCA=int(n_samples*BRCA_rate)
-BRCA=np.array([1]*n_BRCA+[0]*(n_samples-n_BRCA))
-np.random.shuffle(BRCA)
+#US Score based on stage of cancer
+us_score = np.where(diagnosis == 1,
+                    np.random.normal(0.7, 0.15, n),
+                    np.random.normal(0.3, 0.15, n))
+us_score[early_stage_mask] -= np.random.normal(0.1, 0.05, early_stage_mask.sum())
+us_score = np.clip(us_score, 0, 1)
 
-ca125 = np.zeros(n_samples)
+#family history 15% likely to exist among patients with cancer
+family_history = np.random.binomial(1, np.where(diagnosis == 1, 0.15, 0.5), n)
 
-for i in range(n_samples):
-    if Diagnosis[i] == 1:
-        ca125[i] = np.random.normal(100, 50) if np.random.rand() < 0.85 else np.random.normal(20, 10)
-        
-    else:
-        ca125[i] = np.random.normal(20, 10) if np.random.rand() < 0.90 else np.random.normal(60, 20)
-        
 df = pd.DataFrame({
-    'Diagnosis': Diagnosis,
-    'Age': Age.round(),
-    'BMI': BMI.round(1),
-    'menopausal_status': Menopausal,
-    'CA125': ca125.round(1),
+    'Age': age,
+    'CA125': ca125,
+    'TumorSize': tumor_size.round(1),
+    'BRCA': brca,
+    'Menopausal': menopause,
+    'USScore': us_score.round(1),
+    'FamilyHistory': family_history,
+    'Diagnosis': diagnosis
 })
 
-df.to_csv('generated.csv', index=False)
+df.to_csv('synthetic_clinical_dataset.csv', index=False)
